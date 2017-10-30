a127da83ab0c07bec4072f113e488211e5584516
Us <us@Uss-MacBook-Pro.local>
2017-10-29 23:17:52 -0600

    The beginning of the appframe!

--- src/web/src/AppFrame.js
+++ src/web/src/AppFrame.js
@@ -1,5 +1,6 @@
 import { BaseElement, registerElement } from 'single-malt';
 import MainTabs from './MainTabs';
+import AlertBar from './components/AlertBar';
 
 class AppFrame extends BaseElement {
 
@@ -7,13 +8,60 @@ class AppFrame extends BaseElement {
     const template = `
       <style>
         div {
-          width: 200px;
-          height: 200px;
-          background-color: #f0f0f0;
           color: #525252;
+          font-family: "optimusprincepsregular";
+        }
+
+        .app {
+          position: absolute;
+          left: 0;
+          right: 0;
+          top: 0;
+          bottom: 0;
+        }
+
+        .header {
+          position: relative;
+          height: 64px;
+          background-color: black;
+        }
+
+        .logo {
+          background-image: url('assets/sandpoinths.jpg');
+          height: 64px;
+          width: 64px;
+          background-size: contain;
+          float: left;
+        }
+
+        .top-container {
+          display: flex;
+          justify-content: space-between;
+          padding-left: 21px;
+          padding-top: 8px;
+        }
+
+        .title {
+          font-size: 40px;
+          color: white;
+        }
+
+        .login {
+          width: 80px;
+          height: 100%;
         }
       </style>
-      <div>
+      <div class="app">
+        <div class="header">
+          <div class="logo"></div>
+          <div class="top-container">
+            <div class="title">Sandpoint BIC</div>
+            <div class="login">
+              <div>Nate</div>
+            </div>
+          </div>
+        </div>
+        <alert-bar></alert-bar>
         <main-tabs></main-tabs>
         <span class="content">
         </span>
---
+++ src/web/src/components/AlertBar/AlertBadge.js
@@ -0,0 +1,45 @@
+import { BaseElement, registerElement } from 'single-malt';
+
+class AlertBadge extends BaseElement {
+
+  get template() {
+    return `
+      <style>
+        @import url('assets/fonts/bic-icons/bic-icons.css');
+
+        .container {
+          display: flex;
+          padding-left: 12px;
+          padding-right: 12px;
+        }
+
+        .icon {
+          font-size: 36px;
+          padding-right: 12px;
+        }
+
+        .text {
+          text-decoration: underline;
+          display: flex;
+          align-items: center;
+          cursor: pointer;
+        }
+
+        .value {
+          display: flex;
+          align-items: center;
+          font-size: 28px;
+          font-weight: bold;
+          padding-right: 4px;
+        }
+      </style>
+      <div class="container" style="color: {{color}}">
+        <span class="icon {{icon}}"></span>
+        <span class="value">{{value}}</span>
+        <span class="text">{{description}}</span>
+      </div>
+    `;
+  }
+}
+
+export default registerElement()(AlertBadge);
